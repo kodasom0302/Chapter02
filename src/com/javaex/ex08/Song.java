2193702269e4c91af7e067c2407fceeffe0624d2
dasom <dasom589@gmail.com>
2024-01-04 17:02:35 +0900

    2024-01-04 7교시

--- src/com/javaex/ex08/Song.java
+++ src/com/javaex/ex08/Song.java
@@ -2,15 +2,24 @@ package com.javaex.ex08;
 
 public class Song {
 	
-	String title;
-	String artist;
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private int year;
+	private int track;
 	
 	public Song() {
+		System.out.println("Song()");
 	}
 	public Song(String title, String artist) {
 		this.title=title;
 		this.artist=artist;
-		System.out.println(artist+", "+title);
+		this.album=album;
+		this.composer=composer;
+		this.year=year;
+		this.track=track;
+		System.out.println("Song(String title, String artist)");
 	}
 	
 	public String getTitle() {
@@ -25,10 +34,34 @@ public class Song {
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getComposer() {
+		return composer;
+	}
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getTrack() {
+		return track;
+	}
+	public void setTrack(int track) {
+		this.track = track;
+	}
 	
 	@Override
 	public String toString() {
-		return "Song [title=" + title + ", artist=" + artist + "]";
+		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer + ", year="
+				+ year + ", track=" + track + "]";
 	}
-
 }
--- src/com/javaex/ex08/SongApp.java
+++ src/com/javaex/ex08/SongApp.java
@@ -4,8 +4,26 @@ public class SongApp {
 
 	public static void main(String[] args) {
 		
-		Song artist=new Song();
+		Song s1=new Song("좋은날", "아이유");
+		s1.setAlbum("Real");
+		s1.setComposer("이민수");
+		s1.setYear(2010);
+		s1.setTrack(3);
+		System.out.println(s1.toString());
 		
+		Song s2=new Song("거짓말", "BIGBANG");
+		s2.setAlbum("Always");
+		s2.setComposer("G-DRAGON");
+		s2.setYear(2007);
+		s2.setTrack(2);
+		System.out.println(s2.toString());
+		
+		Song s3=new Song("벚꽃엔딩", "버스커버스커");
+		s3.setAlbum("버스커버스커1집");
+		s3.setComposer("장범준");
+		s3.setYear(2012);
+		s3.setTrack(4);
+		System.out.println(s3.toString());
 
 	}
 
