32d1b42fe8349d6796adfb7abca0a37e3869fb5a
dasom <dasom589@gmail.com>
2024-01-04 13:08:41 +0900

    2024-01-04 4교시

---
+++ src/com/javaex/ex07/Point.java
@@ -0,0 +1,49 @@
+package com.javaex.ex07;
+
+public class Point {
+	
+	//필드
+	private int x;
+	private int y;
+	
+	//생성자
+	public Point() {
+		System.out.println("Point()");
+	}
+	public Point(int x) {
+		this.x=x;
+		System.out.println("Point(int x)");
+	}
+	/*
+	public Point(int y) {
+		thisy=y;
+	}
+	위에 int x와 자료형이 똑같아 같은 걸로 취급하여 사용 불가능
+	*/
+	public Point(int x, int y) {
+		this(x);
+		this.y=y;
+		System.out.println("Point(int x, int y)");
+	}
+	
+	//메소드 - g/s
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	//메소드 - 일반
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+	
+}
