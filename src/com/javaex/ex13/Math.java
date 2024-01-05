0ef27dd35387c0e4228e3bb0f72754406a552abf
dasom <dasom589@gmail.com>
2024-01-05 13:00:18 +0900

    2024-01-05 4교시

--- src/com/javaex/ex13/Math.java
+++ src/com/javaex/ex13/Math.java
@@ -3,23 +3,27 @@ package com.javaex.ex13;
 public class Math {
 	
 	//필드
+	private static double pi=3.14;
 	
 	//생성자 : 디폴트 생성자 사용
 	
 	//메소드 - g/s
 	
 	//메소드 - 일반
-	public int plus(int a, int b) {
+	public static int plus(int a, int b) {
 		return a+b;
 	}
-	public double plus(double a, int b) {
+	public static double plus(double a, int b) {
 		return a+b;
 	}
-	public double plus(int a, double b) {
+	public static double plus(int a, double b) {
 		return a+b;
 	}
-	public double plus(double a, double b) {
+	public static double plus(double a, double b) {
 		return a+b;
+	}
+	public static double circleArea(int radius) {
+		return pi*radius*radius;
 	}
 
 }
