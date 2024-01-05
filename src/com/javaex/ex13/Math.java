91fca2109d710d40e452cb9700df136e883b65fe
dasom <dasom589@gmail.com>
2024-01-05 12:22:39 +0900

    2024-01-05 3교시

---
+++ src/com/javaex/ex13/Math.java
@@ -0,0 +1,25 @@
+package com.javaex.ex13;
+
+public class Math {
+	
+	//필드
+	
+	//생성자 : 디폴트 생성자 사용
+	
+	//메소드 - g/s
+	
+	//메소드 - 일반
+	public int plus(int a, int b) {
+		return a+b;
+	}
+	public double plus(double a, int b) {
+		return a+b;
+	}
+	public double plus(int a, double b) {
+		return a+b;
+	}
+	public double plus(double a, double b) {
+		return a+b;
+	}
+
+}
